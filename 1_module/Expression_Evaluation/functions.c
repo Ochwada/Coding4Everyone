7bed21e09395e24489288505d16c12652e9ec337
Ochwada <Ochwada@users.noreply.github.com>
2024-08-31 15:42:54 +0000

    Fundamental Types Declaration and Assignment

--- 1_module/Expression_Evaluation/functions.c
+++ 1_module/Expression_Evaluation/functions.c
@@ -1,3 +1,43 @@
 #include <stdio.h>
 #include "header.h"
 
+void expression_evaluation() {
+    int a = 5;
+    int b = 7;
+    int c = 0;
+    int d = 0;
+
+    c = a - b;
+    printf("a = %d, b = %d, c = %d, d = %d\n", a, b, c, d);
+
+    c = b - a;
+    printf("a = %d, b = %d, c = %d, d = %d\n", a, b, c, d);
+
+    c = a / b; d = b / a;
+    printf("a = %d, b = %d, c = %d, d = %d\n", a, b, c, d);
+
+    c = a % b; d = b % a;
+    printf("a = %d, b = %d, c = %d, d = %d\n", a, b, c, d);
+
+    c = -a - b; d = -b - a;
+    printf("a = %d, b = %d, c = %d, d = %d\n", a, b, c, d);
+
+    c = ++a + b++; d += 5;
+    printf("a = %d, b = %d, c = %d, d = %d\n", a, b, c, d);
+
+}
+
+void type_declaration_assign() {
+    // declare and initialize a variable
+ int a = 5;
+ int b = 7;
+ int c = 6;
+
+ double average = 0.0;// good practice
+
+ printf("a = %d, b = %d, c = %d\n", a, b, c);
+
+ average = (a + b + c) / 3.0;
+ printf("average = %lf\n", average);
+ 
+}
--- 1_module/Expression_Evaluation/header.h
+++ 1_module/Expression_Evaluation/header.h
@@ -1,4 +1,7 @@
 #ifndef HEADER_H
 #define HEADER_H
 
+void expression_evaluation();
+void type_declaration_assign();
+
 #endif 
